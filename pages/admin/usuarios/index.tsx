3643970b564c2cd6fff17d05eafbd92f4c67579c
megateo <camagro.sas@gmail.com>
2022-10-19 18:12:23 -0500

    Esto es nuevo

---
+++ pages/admin/usuarios/index.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import React from 'react';
+
+
+const index = () => {
+  return (
+    <div>
+<div>Pagina de admin de usuarios</div>
+
+<Link href = '/'>
+
+<a>Ir al Home</a>
+
+</Link>
+
+    </div>
+  )
+}
+
+export default index
